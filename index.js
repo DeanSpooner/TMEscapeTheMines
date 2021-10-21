ee353128b84dec95650a69dc4cabe05dd92d5436
DeanSpooner <dean.spooner@live.co.uk>
2021-10-21 15:42:39 +0100

    First tests passed. More complex test reached.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const solve = (map, miner, exit) => {
-  return "right";
+  const totalRight = exit.x - miner.x;
+  const totalDown = exit.y - miner.y;
+  const finalArray = [];
+
+  for (let i = 0; i < totalRight; i++) {
+    finalArray.push("right");
+  }
+
+  for (let i = 0; i < totalDown; i++) {
+    finalArray.push("down");
+  }
+
+  return finalArray;
 };
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -1,10 +1,49 @@
 const { solve } = require("./index.js");
 
-describe("returns a matching number to a numeral", () => {
-  it("returns one simple Roman Numeral's number", () => {
-    const map = [[true], [true]];
-    const miner = { x: 0, y: 0 };
-    const exit = { x: 1, y: 0 };
-    expect(solve(map, miner, exit)).toBe("right");
+describe("returns an array of strings giving directions from the start to finish", () => {
+  it("returns a simple array of 'right' strings", () => {
+    const map1 = [[true], [true]];
+    const miner1 = { x: 0, y: 0 };
+    const exit1 = { x: 1, y: 0 };
+    expect(solve(map1, miner1, exit1)).toEqual(["right"]);
+    const map2 = [[true], [true], [true]];
+    const miner2 = { x: 0, y: 0 };
+    const exit2 = { x: 2, y: 0 };
+    expect(solve(map2, miner2, exit2)).toEqual(["right", "right"]);
+  });
+  it("returns a simple array of 'down' strings", () => {
+    const map1 = [[true, true]];
+    const miner1 = { x: 0, y: 0 };
+    const exit1 = { x: 0, y: 1 };
+    expect(solve(map1, miner1, exit1)).toEqual(["down"]);
+    const map2 = [[true, true, true]];
+    const miner2 = { x: 0, y: 0 };
+    const exit2 = { x: 0, y: 2 };
+    expect(solve(map2, miner2, exit2)).toEqual(["down", "down"]);
+  });
+  it("returns a simple array of 'right' and 'down' strings", () => {
+    const map1 = [
+      [true, false],
+      [true, true],
+    ];
+    const miner1 = { x: 0, y: 0 };
+    const exit1 = { x: 1, y: 1 };
+    expect(solve(map1, miner1, exit1)).toEqual(["right", "down"]);
+    const map2 = [
+      [true, false, false],
+      [true, true, true],
+    ];
+    const miner2 = { x: 0, y: 0 };
+    const exit2 = { x: 1, y: 2 };
+    expect(solve(map2, miner2, exit2)).toEqual(["right", "down", "down"]);
+  });
+  it("returns a simple array of 'right' and 'down' strings that may not go in a simple right-down order", () => {
+    const map1 = [
+      [true, true],
+      [false, true],
+    ];
+    const miner1 = { x: 0, y: 0 };
+    const exit1 = { x: 1, y: 1 };
+    expect(solve(map1, miner1, exit1)).toEqual(["down", "right"]);
   });
 });
